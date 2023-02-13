eba24d34a5d236bb5e1370b6feeb4ad7ad8b8dfe
vnko1 <102794464+vnko1@users.noreply.github.com>
2023-02-13 15:07:39 +0000

    Deploying to gh-pages from @ vnko1/goit-js-hw-11@7596770184eba89b0e99badf2814bf4e7ed6641e 🚀

--- index.edd3ddf5.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault();const{searchQuery:t}=e.currentTarget.elements;console.log(t.value)}));
-//# sourceMappingURL=index.edd3ddf5.js.map
