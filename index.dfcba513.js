a80853af5e8a7503feefa90c2b7268261f78327c
vnko1 <102794464+vnko1@users.noreply.github.com>
2023-02-13 09:57:04 +0000

    Deploying to gh-pages from @ vnko1/goit-js-hw-11@67c4769eb70319c334c97257d84f8b30cea02fb5 🚀

---
+++ index.dfcba513.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault();var r=e.currentTarget.elements.searchQuery;console.log(r.value)}));
+//# sourceMappingURL=index.dfcba513.js.map
---
+++ index.edd3ddf5.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault();const{searchQuery:t}=e.currentTarget.elements;console.log(t.value)}));
+//# sourceMappingURL=index.edd3ddf5.js.map
