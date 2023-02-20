cf524b977307958a9b39c9208517b09af7ea70fc
Andrii Valenko <andreyvalenko@gmail.com>
2023-02-20 12:43:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -113,8 +113,20 @@ function markingUp(data) {
   }, '');
 
   imageContainer.insertAdjacentHTML('beforeEnd', mark);
+  smoothScroll();
 }
 
 function failureLog(message) {
   Notify.failure(message, { clickToClose: true });
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .lastElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 3,
+    behavior: 'smooth',
+  });
 }
